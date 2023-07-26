e5efa5ae87a89951c48e14aee5dc41ea1e0927af
Piotr Dębski <piotrdb1@gmail.com>
2023-07-26 14:20:45 +0200

    add song list items with images

---
+++ components/PageContent.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { Song } from '@/types';
+import SongItem from './SongItem';
+
+type PageContentProps = {
+  songs: Song[];
+};
+
+const PageContent: React.FC<PageContentProps> = ({ songs }) => {
+  if (songs.length == 0) {
+    return <div>There are no available songs right now.</div>;
+  }
+
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 gap-4 mt-4">
+      {songs.map((song) => (
+        <SongItem key={song.id} song={song} onClick={() => {}} />
+      ))}
+    </div>
+  );
+};
+
+export default PageContent;
