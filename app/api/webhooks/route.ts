9e74202ae8c0fa10f1ed98e2b67c206059ce7ce5
Piotr Dębski <piotrdb1@gmail.com>
2023-10-09 18:47:11 +0200

    add create-checkout-session and portal-link routes

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,7 +1,6 @@
 import Stripe from "stripe";
 import { NextResponse } from "next/server";
 import { headers } from "next/headers";
-
 import { stripe } from "@/libs/stripe";
 import {
   upsertProductRecord,
